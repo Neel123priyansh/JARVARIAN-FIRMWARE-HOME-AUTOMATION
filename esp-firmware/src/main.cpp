4c9a31d9e70c4770b234d01f3699581f6804cfa3
Swarnim Burnwal <98446769+SLAYERDAKU@users.noreply.github.com>
2023-12-04 14:18:08 +0530

    api created to turn light on and off

--- esp-firmware/src/main.cpp
+++ esp-firmware/src/main.cpp
@@ -3,30 +3,30 @@
 // Include Functionality according to the board
 #ifdef ESP32
 #include <WiFi.h>
+#include <WebServer.h>
+using ServerType = WebServer;
 #endif
 
 #ifdef ESP8266
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-ESP8266WebServer server(80);
+using ServerType = ESP8266WebServer;
 #endif
 
 //! Define WiFi Credentials (will use env vars in future)
 //! Device is not appearing on router portal
-#define WIFI_SSID "SwastikWiFi"
-#define WIFI_PASS "jarvis@wifi"
+#define WIFI_SSID "yourwifissid"
+#define WIFI_PASS "yourwifipassword"
 #define WIFI_HOSTNAME "IoT-Device"
-#define WIFI_IP IPAddress(192, 168, 0, 240)
-#define WIFI_GATEWAY IPAddress(192, 168, 0, 1)
-#define WIFI_SUBNET IPAddress(255, 255, 255, 0)
-#define WIFI_DNS IPAddress(192, 168, 0, 1) //! Set DNS such that it can't connect to the internet
+
+
+ServerType server(80);
 
 void connectToWiFi() {
   // Connect to Wi-Fi
-  WiFi.config(WIFI_IP, WIFI_GATEWAY, WIFI_SUBNET, WIFI_DNS);
+
   WiFi.setHostname(WIFI_HOSTNAME);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
-
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -34,34 +34,48 @@ void connectToWiFi() {
     Serial.println("Connecting to WiFi...");
   }
 
-  // Connected to WiFi
-  Serial.println("Connected to WiFi");
-  Serial.println(String(WiFi.getHostname()) + "@" + WiFi.localIP().toString());
+    Serial.println("Connected to the WiFi network"+WiFi.localIP().toString());
 }
 
-void handleRoot() {
-  server.send(200, "text/plain", "Hello from " + String(WIFI_HOSTNAME) + "!");
+void handleRoot()
+{
+    server.send(200, "text/plain", "Hello from IoT Device!");
 }
 
+void handle_led()
+{
+    String message = server.arg("command");
+    
+    Serial.println(message);
+    if (message == "on")
+        digitalWrite(LED_BUILTIN, LOW);
+    if (message=="off")
+    {
+        digitalWrite(LED_BUILTIN, HIGH);
+    }
+    String Response = "LED is " + message;
+    server.send(200, "text/plain", Response);
+    
+}
 
 void setup()
 {
-  // Initialize Serial
-  Serial.begin(115200);
-
-  // Connect to WiFi
-  connectToWiFi();
-
-  // Define HTTP endpoint
-  server.on("/", HTTP_GET, handleRoot);
-
-  // Start server
-  server.begin();
-  Serial.println("HTTP server started");
+    Serial.begin(115200);
+    pinMode(2, OUTPUT);
+    digitalWrite(2, HIGH);
+    
+    connectToWiFi();
+    server.on("/",HTTP_GET, handleRoot);
+    server.on("/command",HTTP_POST,handle_led);
+    
+    server.begin();
+    Serial.println("HTTP server started");
+    
 }
-
 void loop()
 {
-  // Handle HTTP requests
-  server.handleClient();
+    server.handleClient();
+    
 }
+
+  
